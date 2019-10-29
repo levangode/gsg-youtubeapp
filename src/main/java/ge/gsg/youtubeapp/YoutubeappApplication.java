9ba4834ef5a504b82c006425f118b8def056ffc4
Levan <lgode14@freeuni.edu.ge>
2019-10-30 00:28:04 +0400

    initial commit

---
+++ src/main/java/ge/gsg/youtubeapp/YoutubeappApplication.java
@@ -0,0 +1,13 @@
+package ge.gsg.youtubeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/gsg/youtubeapp/YoutubeappApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.gsg.youtubeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
