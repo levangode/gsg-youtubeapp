94c1ff03675e266cad62d869b3beeb427d8fc5ae
Levan <lgode14@freeuni.edu.ge>
2019-10-30 21:49:35 +0400

    added the guard to the home page

--- youtubeapp-frontend/src/app/home/home.component.ts
+++ youtubeapp-frontend/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../_services/authentication.service";
+import {User} from "../_models/user";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  currentUser: User;
+  currentUserSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService
+  ) {
+    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
 
   ngOnInit() {
   }
